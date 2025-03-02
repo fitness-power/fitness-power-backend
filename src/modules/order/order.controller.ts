6162e2e48c64d505b789eb7aae6c58cfb3883811
fitness-power <Elnasharm973@gmail.com>
2025-03-02 19:47:41 +0200

    Update order.controller.ts

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -35,7 +35,7 @@ export class OrderController {
   }
   @Public()
   @Post('webhook')
-  async handleStripeWebhook(req: RawBodyRequest<FastifyRequest>) {
+  async handleStripeWebhook(@Req() req: RawBodyRequest<FastifyRequest>) {
     const event = req.rawBody;
     const sig = req.headers['stripe-signature'];
     return this.orderService.handleStripeWebhook(event, sig);
